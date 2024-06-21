1cee854cf79f18befce4b7e21ba3d5012c793f62
Jennifer Albert <jennifer.jalbert3@gmail.com>
2024-06-21 15:21:57 -0400

    Added the start of Auth0 authentication

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using JAlbertHomeMaintenanceCapstone.Components;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+      .AddAuth0WebAppAuthentication(options =>
+      {
+          options.Domain = builder.Configuration["Auth0:Domain"];
+          options.ClientId = builder.Configuration["Auth0:ClientId"];
+      });
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+
+   .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(redirectUri)
+            .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri(redirectUri)
+            .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+    
+app.Run();
