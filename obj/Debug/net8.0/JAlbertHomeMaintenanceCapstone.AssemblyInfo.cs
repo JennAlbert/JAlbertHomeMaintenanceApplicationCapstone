87fe184666612f0de9d6e5a1baf2c15f655d1a61
Jennifer Albert <jennifer.jalbert3@gmail.com>
2024-07-21 09:53:51 -0400

    Fixed form so it will display.

--- obj/Debug/net8.0/JAlbertHomeMaintenanceCapstone.AssemblyInfo.cs
+++ obj/Debug/net8.0/JAlbertHomeMaintenanceCapstone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JAlbertHomeMaintenanceCapstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da320e0b42892131f0d939b23f5405677c0bf671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4671e0b2e64a84a9ce3387bcd26a5d98d710fd9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JAlbertHomeMaintenanceCapstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JAlbertHomeMaintenanceCapstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
